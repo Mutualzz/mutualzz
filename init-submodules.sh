39fb5a692ed1e432afee215691a3e0ae7e00c7a3
Azrael <azrael@mutualzz.com>
2025-07-12 22:38:51 -0700

    removed debugging commands

--- init-submodules.sh
+++ init-submodules.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-git --version
-
 git submodule deinit -f apps/app packages/ui
 git submodule sync
 git submodule update --init --recursive packages/ui apps/app
-
-ls -l packages/ui || true
-ls -l apps/app || true
 
 git submodule status
 
