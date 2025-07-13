4026d48abfd129ec9e44944e27cfc642ae01b8a5
Azrael <azrael@mutualzz.com>
2025-07-12 20:11:36 -0700

    Switched to gitmodules

---
+++ apps/gateway/README.md
@@ -0,0 +1,1 @@
+# Mutalzz Gateway/WS
---
+++ apps/rest/pm2.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "mutualzz-rest",
+            script: "bun",
+            args: ["run", "start"],
+            interpreter: "none",
+        },
+    ],
+};
