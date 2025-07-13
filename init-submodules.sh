96a7fa5142494cfd9858fdac3a62078b5034ecdd
Azrael <azrael@mutualzz.com>
2025-07-12 22:04:19 -0700

    fixing

--- init-submodules.sh
+++ init-submodules.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
+cd "$(dirname "$0")"
 
-cd "${VERCEL_PROJECT_ROOT:-$(git rev-parse --show-toplevel)}"
 
 git --version
 
-git submodule deinit -f apps/app packages/ui || true
-rm -rf apps/app packages/ui
-
-
 git submodule sync
-git submodule update --init --recursive --force apps/app packages/ui
+git submodule update --init --recursive packages/ui apps/app
 
 ls -l packages/ui || true
 ls -l apps/app || true
