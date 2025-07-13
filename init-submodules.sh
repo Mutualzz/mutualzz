714acd1ed751e88e38fbfe408563608c338dbfe5
Azrael <azrael@mutualzz.com>
2025-07-12 21:54:25 -0700

    maybe fixed?

--- init-submodules.sh
+++ init-submodules.sh
@@ -7,15 +7,10 @@ echo "CWD: $(pwd)"
 git --version
 git submodule status
 
-if [ -n "${GITHUB_TOKEN:-}" ]; then
-    git config url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
-fi
-
 # Clean up partial submodules
 git submodule deinit -f .
-git submodule sync --recursive
-
-git submodule update --init --recursive --force
+git submodule sync
+git submodule update --init apps/app packages/ui
 
 ls -l packages || true
 ls -l apps || true
